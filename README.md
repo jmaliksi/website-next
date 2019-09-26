886076ed7679480ea177df7064d2e5af8200d21f
jmaliksi <joe.maliksi@gmail.com>
2019-09-26 09:53:23 -0400

    wordpress integration

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 wobsite
+
+update content
+1. set `WORDPRESS_CLIENT_SECRET`, `WORDPERSS_USER`, `WORDPRESS_PASSWORD`
+1. gatsby build
--- gatsby-config.js
+++ gatsby-config.js
@@ -5,5 +5,37 @@
  */
 
 module.exports = {
-  /* Your site config here */
+  plugins: [
+    {
+      resolve: "gatsby-source-wordpress",
+      options: {
+        // test url
+        baseUrl: "jmaliksitest.wordpress.com",
+        protocol: "http",
+        hostingWPCOM: true,
+        useACF: false,
+        auth: {
+          wpcom_app_clientSecret: process.env.WORDPRESS_CLIENT_SECRET,
+          wpcom_app_clientID: "66979",
+          wpcom_user: process.env.WORDPRESS_USER,
+          wpcom_pass: process.env.WORDPRESS_PASSWORD
+        },
+        verboseOutput: false,
+        perPage: 100,
+        searchAndReplaceContentUrls: {
+          sourceUrl: "https://jmaliksitest.wordpress.com",
+          replacementUrl: "https://localhost:8000",
+        },
+        concurrentRequests: 10,
+        includedRoutes: [
+          "**/categories",
+          "**/posts",
+          "**/pages",
+          "**/media",
+          "**/tags",
+          "**/taxonomies",
+        ]
+      }
+    }
+  ]
 }
