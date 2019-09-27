940c193abb645bd261e450b17f00cd514f2869f5
jmaliksi <joe.maliksi@gmail.com>
2019-09-27 14:35:06 -0400

    about page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,48 @@
 import React from "react"
+import { Link, graphql } from "gatsby"
 import Layout from "../components/layout"
+import styles from "../styles/index.module.css"
 
-export default () => (
+function ContactInfo() {
+  return (
+    <div>
+      <h2>contact</h2>
+      <div className={styles.contactSection}>
+        <div className={styles.contactLabel}>come talk</div>
+        <div className={styles.contactInfo}>
+          <ul>
+            <li>joe [dot] maliksi [at] gmail [dot] com</li>
+          </ul>
+        </div>
+      </div>
+      <div className={styles.contactSection}>
+        <div className={styles.contactLabel}>or just stalk</div>
+        <div className={styles.contactInfo}>
+          <ul>
+            <li><a href="https://twitter.com/ch00beh">Twitter</a></li>
+            <li><a href="http://www.linkedin.com/pub/joseph-maliksi/35/b/903">LinkedIn</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ({data}) => (
   <Layout highlight="About">
-    <div>Hello world!</div>
+    <div className={styles.content}>
+      <h1>hi</h1>
+      <h2>about</h2>
+      <div dangerouslySetInnerHTML={{ __html: data.wordpressPage.content }} />
+      <ContactInfo />
+    </div>
   </Layout>
 )
+
+export const query = graphql`
+  query {
+    wordpressPage(slug: {eq: "about"}) {
+      content
+    }
+  }
+`
