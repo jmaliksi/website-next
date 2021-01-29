a96a65e929e0e56a9a9ca87981b228bb3cc72756
joe <joe.maliksi@gmail.com>
2021-01-29 14:18:24 -0500

    sidebar images

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -2,10 +2,17 @@ import React from "react"
 import styles from "./sidebar.module.css"
 
 function SidebarEntry(props) {
-  const { slug, title } = props.post;
+  const { slug, title, jetpack_featured_media_url } = props.post;
   return (
     <li>
-      <a href={`#${slug}`} className={styles.sidebarLink}>{title}</a>
+      <a href={`#${slug}`} className={styles.sidebarLink}>
+        <div>
+          {title}
+          {jetpack_featured_media_url &&
+            <img alt={slug} src={jetpack_featured_media_url} className={styles.thumb}/>
+          }
+        </div>
+      </a>
     </li>
   );
 }
