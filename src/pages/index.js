95468f94fea05ef4d4f270fc641194770b422e94
joe <joe.maliksi@gmail.com>
2021-01-31 00:02:05 -0500

    pfp stuff

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,54 @@ import { graphql } from "gatsby"
 import Layout from "../components/layout"
 import styles from "../styles/index.module.css"
 
+
+class CharacterSheet extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {showImg: true, page: 0};
+    this.pages = 2;
+    this.stats = [];
+    React.Children.forEach(props.children, c => {
+      this.stats.push(c.props.children);
+    });
+  }
+
+  figure() {
+    return (
+      <figure>
+        <img src="https://jmaliksitest.files.wordpress.com/2021/01/a0a37a8d-3390-46b9-83f2-9b520987c568_1_105_c.jpeg"/>
+        <figcaption>
+          pfp by <a href="https://twitter.com/ouroborose">Rose Peng</a>
+        </figcaption>
+      </figure>
+    );
+  }
+
+  renderStats() {
+    return (
+      <ul>
+      {this.stats.map(stat => {return <li key={stat}>{stat}</li>})}
+      </ul>
+    );
+  }
+
+  showStats = () => {
+    this.setState({showImg: false});
+  }
+
+  hideStats = () => {
+    this.setState({showImg: true});
+  }
+
+  render = () => {
+    return (
+      <div className={styles.characterSheet} onMouseEnter={this.showStats} onMouseLeave={this.hideStats}>
+        {this.state.showImg ? this.figure() : this.renderStats()}
+      </div>
+    );
+  }
+}
+
 class Email extends React.Component {
   constructor(props) {
     super(props);
@@ -63,7 +111,25 @@ export default ({data}) => (
     <div className={styles.content}>
       <h1>hi</h1>
       <h2 id="about">about</h2>
-      <div dangerouslySetInnerHTML={{ __html: data.wordpressPage.content }} />
+      <CharacterSheet>
+        <li>wrangling: +2</li>
+        <li>creativity: +1</li>
+        <li>programming: +3</li>
+        <li>jokes: +3</li>
+        <li>impulse control: -1</li>
+        <li>lawyer: -1</li>
+        <li>napping: -2</li>
+        <li>relaxing: +2</li>
+        <li>balance: +1</li>
+        <li>speaking: -1</li>
+        <li>quietness: -1</li>
+        <li>crafting: +2</li>
+        <li>friend shape: +2</li>
+        <li>cooking: +1</li>
+        <li>executive function: -2</li>
+        <li>computers: -0</li>
+      </CharacterSheet>
+      <div dangerouslySetInnerHTML={{ __html: data.wordpressPage.content }} className={styles.about}/>
       <ContactInfo />
     </div>
   </Layout>
